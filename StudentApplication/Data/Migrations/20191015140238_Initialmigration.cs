4fc4f479d9c66ae8900f71c59345296cee7afe1e
oluxell.voa007@gmail.com <oluxell.voa007@gmail.com>
2019-10-15 18:44:06 -0700

    Add project files.

---
+++ StudentApplication/Data/Migrations/20191015140238_Initialmigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentApplication.Data.Migrations
+{
+    public partial class Initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Firstname = table.Column<string>(nullable: false),
+                    MiddleName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: false),
+                    DateOfBirth = table.Column<string>(nullable: true),
+                    Score = table.Column<int>(nullable: false),
+                    Grade = table.Column<string>(nullable: true),
+                    StudentNo = table.Column<int>(nullable: false),
+                    Course = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ StudentApplication/Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApplication.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required ]
+        [Display(Name = "First Name")]
+        public string Firstname { get; set; }
+
+        [Display(Name = "MiddleName")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date Of Birth")]
+        public string DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "Score")]
+        public int Score { get; set; }
+
+        [Display(Name = "Grade")]
+        public string Grade { get; set; }
+
+        [Display(Name = "Student No")]
+        public int StudentNo { get; set; }
+
+        [Display(Name ="Course")]
+        public string Course { get; set; }
+    }
+}
